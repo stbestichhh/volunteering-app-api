fae12f54e7077ef5676fbb45b72da1a03d648b6e
Kiril Yakymchuk <stbestich@gmail.com>
2024-11-03 11:53:20 +0200

    feat: pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from '@app/common/logger';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get<Logger>(Logger));
+
   await app.listen(3000);
 }
 bootstrap();
